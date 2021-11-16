d8fbddea122b6566ca903dd71774418bf1cc413d
Tymivv <if210683tvn@gmail.com>
2021-11-16 16:39:13 +0200

    зробив перше завдання

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
 
+
+const bodyRef = document.querySelector('body');
+const btrRef = document.querySelectorAll('button');
+
+
+[...btrRef].forEach(item => {
+    item.addEventListener('click', abdateValue);
+    
+});
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+let activeBtr = false;
+function abdateValue(e) {
+     //console.log();
+    if (e.target.dataset.hasOwnProperty("start") && activeBtr === false) {
+        
+        timerId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+    
+        }, 1000);
+        activeBtr = true;
+    }
+    if (e.target.dataset.hasOwnProperty('stop')) {
+        clearInterval(timerId);
+        activeBtr = false;
+    }
+};
+
+
+
